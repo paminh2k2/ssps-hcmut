942ee262f473ac26e3da067934645f15cf1cfe07
minhpham2k2 <aminh252781@gmail.com>
2023-12-11 20:37:41 +0700

    update css

--- src/component/CalendarConfig/CalendarConfig.jsx
+++ src/component/CalendarConfig/CalendarConfig.jsx
@@ -1,16 +1,19 @@
+import classNames from 'classnames/bind';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import "./calendar.css";
+import styles from './calendar.module.scss';
+const cx = classNames.bind(styles)
+
 export default function CalendarConfig() {
     const [date, setDate] = useState(new Date());
     const onChange= date =>{
         setDate(date);
     };
     return (
-        <div>
+        <div className={cx('div')}>
             {/* <h2>Date Issue</h2> */}
-            <div className='datebox'>{date.toDateString()}</div>
+            <div className={cx('datebox')}>{date.toDateString()}</div>
             <Calendar
                 onChange={onChange}
                 value={date}
